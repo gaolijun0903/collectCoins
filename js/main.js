10ad4db5a71b74e7f609889992c4f948de59574c
gaolijun <15801419993@163.com>
2018-01-23 18:01:10 +0800

    aaa

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var states = {
 	        game.stage.backgroundColor = '#FFFFFF';
 	        // 加载游戏资源
 	        game.load.crossOrigin = 'anonymous'; // 设置跨域
-	        game.load.image('bg1', 'images/bg1.png');//首页-背景
+	        game.load.image('homepagebg', 'images/homepagebg.png');//首页-背景
 	        game.load.image('title', 'images/title.png');  //首页-标题
 	        game.load.image('startbtn', 'images/startbtn.png'); //首页-开始游戏按钮
 	        game.load.image('rulebtn', 'images/rulesbtn.png'); //首页-活动规则按钮
@@ -24,9 +24,15 @@ var states = {
 	        game.load.image('sharebtn', 'images/sharebtn.png'); //首页-分享按钮 、  结束页-分享按钮
 	        game.load.spritesheet('dude', 'images/dude.png', 32, 48); //游戏主角
 	        
-	        game.load.image('bg', 'images/bg.png');//游戏页-背景
-	        game.load.image('header', 'images/header.png');//游戏页-头部背景
-	        game.load.image('star', 'images/star.png'); //游戏页-星星
+	        game.load.image('playbg', 'images/playbg.png');//游戏页-背景
+	        game.load.image('timerbg', 'images/timerbg.png');//游戏页-定时器背景
+	        game.load.image('playcount', 'images/playcount.png');//游戏页-游戏次数背景
+	        game.load.image('coinbg', 'images/coinbg.png');//游戏页-金币数背景
+	        game.load.image('coin', 'images/coin.png'); //游戏页-金币
+	        game.load.image('stone', 'images/stone.png'); //游戏页-
+	        game.load.image('roadblock', 'images/roadblock.png');//游戏页-
+	        game.load.image('garbagecan', 'images/garbagecan.png');//游戏页-
+	        
 	        game.load.image('bomb', 'images/bomb.png'); //游戏页-炸弹
 	        game.load.image('five', 'images/five.png'); //游戏页-加分图片 （开始倒计时暂用）
 	        game.load.image('three', 'images/three.png');//游戏页-加分图片 （开始倒计时暂用）
@@ -63,8 +69,9 @@ var states = {
     	this.create = function() {
     		// 声音管理类 
     		this.soundManager = game.sound;
+    		
             // 添加背景
-	        var bg = game.add.image(0, 0, 'bg1');
+	        var bg = game.add.image(0, 0, 'homepagebg');
 	        bg.width = game.world.width;
 	        bg.height = game.world.height;
 	        
@@ -84,7 +91,8 @@ var states = {
 	            fill: '#f2bb15'
 	        });
 	        title.anchor.setTo(0.5, 0.5);
-	       
+	        
+	      
 	        // 添加主角
 	        var man = game.add.sprite(game.world.centerX, game.world.height * 0.75, 'dude');
 	        var manImage = game.cache.getImage('dude');
@@ -122,7 +130,7 @@ var states = {
 	        	alert('分享')
 	        }
        },
-       this.judgeMute = function(update){
+        this.judgeMute = function(update){
 	    	if(update){
 	    		if(this.soundManager.mute){
 	        		muteButton.angle = 0;
@@ -144,14 +152,13 @@ var states = {
     		scoreMusic,
         	bombMusic,
         	bgMusic,
-        	muteButton;
-        // 是否正在触摸
-		var touching = false;
-        var preX = 0;
-    	// 障碍物和奖励的速度
-		var move_velocity = 200;
-		// x滑动的最小触发距离
-		var minTouchDis = width / 8;
+        	muteButton, 
+        	preX = 0,
+        	touching = false, // 是否正在触摸
+        	move_velocity = 200, // 障碍物和奖励的速度
+        	minTouchDis = width / 8, // x滑动的最小触发距离
+        	obstaclesTypes = ['stone','roadblock','garbagecan'];
+       
     	this.create = function(){
     		// 声音管理类
     		this.soundManager = game.sound;
@@ -164,11 +171,10 @@ var states = {
             scoreMusic = game.add.audio('scoreMusic');
             bombMusic = game.add.audio('bombMusic');
     		// 添加背景
-	        var bg = game.add.image(0, 0, 'bg');
+	        var bg = game.add.image(0, 0, 'playbg');
 	        bg.width = game.world.width;
 	        bg.height = game.world.height;
-    		// 开启物理世界
-	    	//game.physics.startSystem(Phaser.Physics.ARCADE);
+    		
 	    	//添加主角
 	        this.car = this.game.add.sprite(32, game.world.height - 150, 'dude');
 	        var carWidth = this.car.width;
@@ -189,47 +195,40 @@ var states = {
 	  		this.car.animations.add('right', [5, 6, 7, 8], 10, true);
 	  		this.car.animations.play('center');
 	  		
-	  		// 创建一个group，包含20个障碍物
+	  		
+	        // 创建一个group，包含coin  stone  roadblock  garbagecan
 	        this.obstacles = game.add.group();
 	        this.obstacles.enableBody = true;
-	        this.obstacles.createMultiple(15, 'bomb');
-	        this.obstacles.forEach(function(item){
-	        	item.width = 50;
-	        	item.height= 50;
-	        	item.body.setSize(40,40,0,0);
-	        	// kill超出边界的障碍物
-		        item.checkWorldBounds = true;
-		        item.outOfBoundsKill = true;
-	        })
-	        
-	        // 创建一个group，包含10个奖励星星
-	        this.stars = game.add.group();
-	        this.stars.enableBody = true;
-	        this.stars.createMultiple(20, 'star');
-	        this.stars.forEach(function(item){
-	        	// kill超出边界的障碍物
-		        item.checkWorldBounds = true;
-		        item.outOfBoundsKill = true;
-	        })
 	       
-	       	// 添加头部背景
-	        var bg = game.add.image(0, 0, 'header');
-	        bg.width = game.world.width;
-	        bg.height = 90;
-	        
+	       	// 添加时间背景
+	        var timerbg = game.add.image(19, 16, 'timerbg');
+	        timerbg.width = 112;
+	        timerbg.height = 41;
 	        // 添加时间
 			this.remainTime = 60;
 	        var style = { font: "20px Arial", fill: "#ffffff" };
-	        this.remainTimeText = this.game.add.text(20, 20, "时间: "+this.remainTime, style);
+	        this.remainTimeText = this.game.add.text(62, 25, "01：00", style);
 			
+			// 添加次数背景
+	        var timerbg = game.add.image(game.world.centerX-25, 16, 'playcount');
+	        timerbg.width = 49;
+	        timerbg.height = 49;
+	        // 添加次数
+			this.remainCount = 3;
+	        var style = { font: "22px Arial", fill: "#ffffff" };
+	        this.remainCountText = this.game.add.text(game.world.centerX, 25, this.remainCount, style);
+	        
+			// 添加分数背景
+	        var coinbg = game.add.image(game.world.width-19-112, 16, 'coinbg');
+	        coinbg.width = 112;
+	        coinbg.height = 41;
 			// 添加分数
 			this.score = 0;
 	        var style = { font: "20px Arial", fill: "#ffffff" };
-	        this.scoreText = this.game.add.text(20, 20, "分数: "+this.score, style);
-	        this.scoreText.x = game.world.width - this.scoreText.width - this.scoreText.width;
+	        this.scoreText = this.game.add.text(game.world.width-19-90, 25, " "+this.score, style);
 	        
 	        // 添加静音按钮  播放
-	        muteButton = game.add.button(game.world.centerX, 30, 'mute-play', onMute, this, 0, 0, 0);
+	        muteButton = game.add.button(game.world.width-19, 40, 'mute-play', onMute, this, 0, 0, 0);
 	        this.judgeMute();
 	        muteButton.anchor.setTo(0.5, 0.5);
 	        function onMute(){
@@ -241,10 +240,7 @@ var states = {
 			this.game.input.addMoveCallback(this.moveCallback,this);
 			// 监听按下事件
 			game.input.onDown.add(function(pointer) {
-			    // 要判断是否点住主角，避免瞬移
-    			if (Math.abs(pointer.x - this.car.x) < this.car.width / 2 && Math.abs(pointer.y - this.car.y) < this.car.height / 2){
-    				 touching = true;
-    			}
+				touching = true;
 			},this);
 			// 监听离开事件
 			this.game.input.onUp.add(function(pointer) {
@@ -270,8 +266,6 @@ var states = {
     		this.judgeMute(1);
     		// 小车和障碍物的碰撞监听
     		game.physics.arcade.overlap(this.car, this.obstacles, this.crashCarFunc, null, this);
-    		// 小车和奖励的碰撞监听
-    		game.physics.arcade.overlap(this.car, this.stars, this.eatStarFunc, null, this);
     	},
     	this.moveCallback = function(pointer, x, y, isTap) {
 			if (isTap || !touching) return
@@ -349,46 +343,43 @@ var states = {
 	    	}
         },
 	    this.add_move_sprite = function(){
-	    	// 随机[0,10)的整数，确定是创建障碍物还是星星
 	    	var starNum = Math.floor(Math.random()*10);
-	    	if(starNum <= 1){
-	    		this.add_n_star(2);
-	    	}else if(starNum <= 4){
-	    		this.add_n_star(1);
+	    	//console.log(starNum)
+	    	if(starNum <= 3){
+	    		this.addmystones('coin',1);
+	    	}else if(starNum <= 5){
+	    		this.addmystones('coin',2);
+	    	}else if(starNum <= 6){
+	    		this.addmystones('stone',1);
+	    	}else if(starNum <= 8){
+	    		this.addmystones('roadblock',1);
 	    	}else{
-	    		this.add_one_obstacle();
+	    		this.addmystones('garbagecan',1);
 	    	}
 	    },
-	    this.add_one_obstacle = function(){
-	    	// 从group中获取第一个死亡的对象
-	        var obstacle = this.obstacles.getFirstDead();
-	        if(obstacle){
-	        	// 障碍物从跑道的3个位置掉落
-	        	// 随机[0,2]的整数,确定下落的跑道
-	    		var num = Math.floor(Math.random()*3);
-	        	var	halfRoadWidth = (game.world.width-grassBeltWidth*2)/6;
-	        	var x = grassBeltWidth+ halfRoadWidth*(num*2+1)-obstacle.width/2;
-		  		var y = -obstacle.height;
-	        	// 重新设置位置
-		        obstacle.reset(x, y);
-	        }
-	    },
-	    this.add_n_star = function(n){
-	     	// 障碍物从跑道的3个位置掉落
-        	// 随机[0,2]的整数,确定下落的跑道
-    		var num = Math.floor(Math.random()*3);
-	     	for(var i=0;i<n;i++){
-	     		// 从group中获取第一个死亡的对象
-		        var star = this.stars.getFirstDead();
-		        if(star){
-		    		var	halfRoadWidth = (game.world.width-grassBeltWidth*2)/6;
-		    		var x = grassBeltWidth+ halfRoadWidth*(num*2+1)-star.width/2;
-			  		var y = (star.height+10)*(n-i-1);;
-		        	// 重新设置位置
-			        star.reset(x, y);
-		        }
-	     	}
-	    },
+	    this.addmystones = function(type,n){
+	    	// 随机[0,2]的整数,确定下落的跑道
+		    var num = Math.floor(Math.random()*3);
+	    	for(var i=0;i<n;i++){
+		    	// 从group中获取第一个死亡的对象
+		        //var obstacle = this.obstacles.getFirstDead();
+		        //if(obstacle){
+			  		var obstacle = this.obstacles.create(0, 0, type);
+			  		obstacle.width = 50;
+		        	obstacle.height= 50;
+		        	obstacle.body.setSize(40,40,0,0);
+		        	// kill超出边界的障碍物
+			        obstacle.checkWorldBounds = true;
+			        obstacle.outOfBoundsKill = true;
+			  		obstacle.type = type;
+		        	var	halfRoadWidth = (game.world.width-grassBeltWidth*2)/6;
+		        	var x = grassBeltWidth+ halfRoadWidth*(num*2+1)-obstacle.width/2;
+			  		var y = -obstacle.height;
+			  		// 重新设置位置
+			        obstacle.reset(x, y);
+			    //}   
+			}
+       	},
 	    this.timerCallback = function(){
 	    	this.add_move_sprite();
 	    	this.reduceTime();
@@ -396,15 +387,14 @@ var states = {
 	    this.reduceTime = function(){
 	    	this.remainTime -= 0.5;
 	    	//console.log(this.remainTime+"===="+parseInt(this.remainTime));
-	        this.remainTimeText.text = "时间: "+parseInt(this.remainTime);
+	    	var timeStr = parseInt(this.remainTime)<10 ? '0'+parseInt(this.remainTime) : parseInt(this.remainTime);
+	        this.remainTimeText.text = "00: "+timeStr;
 	        //随着时间进行，速度越来越快
 	        var v = move_velocity + (60-this.remainTime)*20;
 	        this.obstacles.forEachAlive(function(item){
 	    		item.body.velocity.y = v;
 	    	});
-	    	this.stars.forEachAlive(function(item){
-	    		item.body.velocity.y = v;
-	    	});
+
 	        // 结束场景
 	        if(this.remainTime <= 0){ 
 	        	this.allStopMove();
@@ -422,22 +412,30 @@ var states = {
 	    	this.obstacles.forEach(function(item){
 	    		item.body.velocity.y = 0;
 	    	})
-	    	this.stars.forEach(function(item){
-	    		item.body.velocity.y = 0;
-	    	})
 	    	//取消滑动监听，主角不可移动
 	    	this.game.input.deleteMoveCallback(this.moveCallback,this);
 	    },
 	    this.crashCarFunc = function(car, obstacle){
 	    	obstacle.kill();
-	    	this.allStopMove();
-	    	// 播放音效
-    		bombMusic.play();
-	    	car.animations.play('right');
+	    	var imageName = '';
+	    	if(obstacle.type==='coin'){
+	    		imageName = 'one';
+	    		// 更新分数
+			   	this.score += 1;
+	        	this.scoreText.text =  this.score; 
+			    // 播放音效
+	    		scoreMusic.play();
+	    	}else{
+	    		imageName = 'five';
+	    		this.allStopMove();
+	    		// 播放音效
+	    		bombMusic.play();
+		    	car.animations.play('right');
+	    	}
 	    	
 	    	// 添加爆炸图片
-		    var goal = game.add.image(obstacle.x, obstacle.y, 'five');
-		    var goalImg = game.cache.getImage('five');
+		    var goal = game.add.image(obstacle.x, obstacle.y, imageName);
+		    var goalImg = game.cache.getImage(imageName);
 		    goal.width = obstacle.width;
 		    goal.height = goal.width / (goalImg.width / goalImg.height);
 		    goal.alpha = 0;
@@ -454,40 +452,11 @@ var states = {
 		        }, 100, Phaser.Easing.Linear.None, true, 200, 0, false);
 		        hideTween.onComplete.add(function() {
 		            goal.kill();
+		            if(obstacle.type==='coin') return
 		            game.state.start('over', true, false, that.score); 
 		        });
 		    });
 		    //在此把分数发送给后台
-	    },
-	    this.eatStarFunc = function(car, star){
-	    	//console.log("吃到奖励+加分");
-	        // 添加得分图片
-		    var goal = game.add.image(star.x, star.y, 'one');
-		    var goalImg = game.cache.getImage('one');
-		    goal.width = star.width;
-		    goal.height = goal.width / (goalImg.width / goalImg.height);
-		    goal.alpha = 0;
-		    // 添加过渡效果
-		    var showTween = game.add.tween(goal).to({
-		        alpha: 1,
-		        y: goal.y - 20
-		    }, 100, Phaser.Easing.Linear.None, true, 0, 0, false);
-		    showTween.onComplete.add(function() {
-		        var hideTween = game.add.tween(goal).to({
-		            alpha: 0,
-		            y: goal.y - 20
-		        }, 100, Phaser.Easing.Linear.None, true, 200, 0, false);
-		        hideTween.onComplete.add(function() {
-		            goal.kill();
-		        });
-		    });
-		    // 更新分数
-		   	this.score += 1;
-        	this.scoreText.text = '分数: ' + this.score; 
-		    // 清除star
-		    star.kill();
-		    // 播放音效
-    		scoreMusic.play();
 	    }
     },
     // 结束场景
@@ -499,8 +468,9 @@ var states = {
 	    this.create = function() {
 	    	// 声音管理类
     		this.soundManager = game.sound;
+    		
 	        // 添加背景
-	        var bg = game.add.image(0, 0, 'bg1');
+	        var bg = game.add.image(0, 0, 'homepagebg');
 	        bg.width = game.world.width;
 	        bg.height = game.world.height;
 	        
---
+++ weixin/weixin.js
@@ -0,0 +1,132 @@
+<!--端内分享相关-->
+	
+	    //document.cookie='_app_token_v3=ZNLbf6_g1Ur4GbsaVVdXwJRkPnJhpbcV84O_Es4ySYI';//TODO
+	    function ajaxShare(obj){
+	        // 分享变量设置
+	        var _protocolLink = "yongche://share?";
+	        var _protocolLinkFriend = "yongche://shareToFriend?";
+	        var _protocolLinkTimeline = "yongche://shareToTimeline?";
+	
+	        // 分享地址
+	        //var query = encodeURIComponent("&language="+obj.activity_language+"&sendName="+obj.sendName+"&sendMoney="+obj.sendMoney+"&sendIphone="+obj.sendIphone+"&headImg="+obj.headImg);
+	        var shareRead_link = encodeURIComponent(obj.url);
+	
+	        // 分享的图片
+	        var shareRead_pics = encodeURIComponent("http://i3.yongche.Name/media/g2/M01/1A/35/rBEBJVo82ViIBg3PAAAydsM-krwAAKcAAK_ajsAADKO105.jpg");
+	
+	        // 分享的标题
+	        var shareRead_title = encodeURIComponent(obj.title);
+	        // 分享的内容
+	        var shareRead_content = encodeURIComponent(obj.description);
+	        // 分享的需要生成二维码地址
+	        var shareRead_codeUrl = encodeURIComponent("://m.yongche.com");
+	        var shareRead_sourceType = 42; //1-截图分享 42-图文分享
+	        var shareRead_callback = "share_callback";
+	
+	        var Tools = {
+	            shareThisPage:function(){
+	                var _str = "link="+shareRead_link+"&pics="+shareRead_pics+"&title="+shareRead_title+"&content="+shareRead_content+"&sourceType="+shareRead_sourceType+"&callback="+shareRead_callback+"&codeUrl="+shareRead_codeUrl;
+	                var _tmpOpenLink = _protocolLink + _str;
+	                window.location.href = _tmpOpenLink;
+	            },
+	            shareThisPageToFriend:function(){
+	                var _str = "link="+shareRead_link+"&pics="+shareRead_pics+"&title="+shareRead_title+"&content="+shareRead_content+"&sourceType="+shareRead_sourceType+"&callback="+shareRead_callback+"&codeUrl="+shareRead_codeUrl;
+	                var _tmpOpenLink = _protocolLinkFriend + _str;
+	                window.location.href = _tmpOpenLink;
+	            },
+	            shareThisPageToTimeline:function(){
+	                var _str = "link="+shareRead_link+"&pics="+shareRead_pics+"&title="+shareRead_title+"&content="+shareRead_content+"&sourceType="+shareRead_sourceType+"&callback="+shareRead_callback+"&codeUrl="+shareRead_codeUrl;
+	                var _tmpOpenLink = _protocolLinkTimeline + _str;
+	                window.location.href = _tmppenLOink;
+	            }
+	        };
+	        var _str = "link="+shareRead_link+"&pics="+shareRead_pics+"&title="+shareRead_title+"&content="+shareRead_content+"&sourceType="+shareRead_sourceType+"&from=iframe&callback="+shareRead_callback;
+	        var _tmpOpenLink = _protocolLink + _str;
+	        console.log(_tmpOpenLink);
+	//        $('.share').delegate('.share-btn', 'click', function() {
+	//            ga('send', 'event', 'share_button', 'click', 'shareClickChristmas');
+	//            Tools.shareThisPage();
+	//        });
+	        document.getElementById("share-btn").addEventListener("click", function(){
+	            ga('send', 'event', 'share_button', 'click', 'shareClickChristmas');
+	            Tools.shareThisPage();
+	        });
+	
+	    }
+	
+	
+		
+	<!--微信二次分享-->	
+	
+       
+        
+        (function ($) {
+            $.fn.wxShare = function (oParam) {
+                var shareImg = oParam.shareImg;
+                var shareTitle = oParam.shareTitle;
+                var shareContent = oParam.shareContent;
+                var shareUrl = oParam.shareUrl;
+                $.ajax({
+                    url:"//weixin.yongche.com/wechat/jssdk/config?url="+encodeURIComponent(window.location.href),
+                    method:'get',
+                    dataType:'jsonp'
+                }).done(function(data){
+                    console.log(data);
+                    if(data.code==200){
+                        wx_share(data.result);
+                    }
+                }).fail(function(error){
+                    //console.log(error)
+                });
+                function wx_share(jsData) {
+                    wx.config({
+                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        appId: jsData.appId, // 必填，公众号的唯一标识
+                        timestamp: jsData.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: jsData.nonceStr, // 必填，生成签名的随机串
+                        signature: jsData.signature,// 必填，签名，见附录1
+                        jsApiList: ['onMenuShareAppMessage','onMenuShareTimeline','onMenuShareQQ','onMenuShareWeibo'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+                    });
+                    wx.ready(function(){
+                        var shareData =
+                            {
+                                link:shareUrl,
+                                imgUrl:shareImg,
+                                title:shareTitle,
+                                desc:shareContent,
+                                type: '', // 分享类型,music、video或link，不填默认为link
+                                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                                success: function () {
+                                    // 用户确认分享后执行的回调函数
+                                },
+                                cancel: function () {
+                                    // 用户取消分享后执行的回调函数
+                                }
+                            };
+                        wx.onMenuShareAppMessage(shareData);
+                        wx.onMenuShareTimeline(shareData);
+                        wx.onMenuShareQQ(shareData);
+                        //alert('ok');
+                    });
+                }
+            }
+
+            window.wxShareFn = function(oParam){
+                $.fn.wxShare(oParam);
+            }
+
+        })(jQuery);
+    		
+		var wShare = {};
+        wShare.shareImg     = 'http://i2.yongche.Name/media/g2/M03/1A/35/rBEBJVo84_iIZPtgAAAydsM-krwAAKcBQLESpgAADKO907.jpg';
+        wShare.shareTitle   = '来自易到的圣诞邀请';
+        wShare.shareContent = '双人游，iPhone X等大奖送不停！';
+        wShare.shareUrl = window.location.href;
+        wxShareFn(wShare);
+        
+        
+	<!--GA-->
+	
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-18761483-4', 'auto');  ga('send', 'pageview');
+  
